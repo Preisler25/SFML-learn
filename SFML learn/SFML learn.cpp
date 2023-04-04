07e7dba19cfe9cce1b569abeab6dac88beab1e9b
Preisler <preisler.andras@gmail.com>
2023-04-04 11:31:53 +0200

    main

--- SFML learn/SFML learn.cpp
+++ SFML learn/SFML learn.cpp
@@ -3,49 +3,52 @@
 
 using namespace std;
 
-sf::Vector2f moveUp(sf::Vector2f vect) {
-    vect.y += 10;
-    cout << "W";
-    return vect;
-}
-
-sf::Vector2f moveDown(sf::Vector2f vect) {
+void moveUp(sf::Vector2f&vect) {
     vect.y -= 10;
-    cout << "S";
-    return vect;
+
 }
 
-sf::Vector2f moveLeft(sf::Vector2f vect) {
+void moveDown(sf::Vector2f&vect) {
+    vect.y += 10;
+}
+
+void moveLeft(sf::Vector2f&vect) {
     vect.x -= 10;
-    cout << "A";
-    return vect;
 }
 
-sf::Vector2f moveRight(sf::Vector2f vect) {
+void moveRight(sf::Vector2f&vect) {
     vect.x += 10;
-    cout << "D";
-    return vect;
 }
 
+void chColor(sf::RectangleShape&rect) {
+    sf::Color color = rect.getFillColor();
+    if (color == sf::Color::Cyan) {
+        rect.setFillColor(sf::Color::Red);
+    }
+    else
+    {
+        rect.setFillColor(sf::Color::Cyan);
+    }
+}
 
 
 int main()
 {
-    const float x_side = 300;
+    const float x_side = 100;
     const float y_side = 100;
 
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "My game");
+    sf::RenderWindow window(sf::VideoMode(1200, 700), "My game");
     window.setFramerateLimit(60);
     
     sf::RectangleShape rect;
 
-    sf::Vector2f rectPosition(600, 350);
+    sf::Vector2f rectPosition(600, 400);
 
     rect.setSize(sf::Vector2f(x_side, y_side));
 
 
-    float xVelocity = 3;
-    float yVelocity = 3;
+    float xVelocity = 10;
+    float yVelocity = 10;
 
 
     while (window.isOpen())
@@ -57,20 +60,24 @@ int main()
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) rectPosition = moveUp(rectPosition);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) moveUp(rectPosition);
             
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) rectPosition = moveDown(rectPosition);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) moveDown(rectPosition);
             
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) rectPosition = moveLeft(rectPosition);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) moveLeft(rectPosition);
             
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) rectPosition = moveRight(rectPosition);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) moveRight(rectPosition);
 
 
         }
 
         //physics
-        if (rectPosition.x < 0 || rectPosition.x > 1280 - x_side) xVelocity *= -1;
-        if (rectPosition.y < 0 || rectPosition.y > 720 - y_side) yVelocity *= -1;
+        if (rectPosition.x == 0 && rectPosition.y == 00) chColor(rect);
+        if (rectPosition.x == 1200 && rectPosition.y == 00) chColor(rect);
+        if (rectPosition.x == 0 && rectPosition.y == 700) chColor(rect);
+        if (rectPosition.x == 1200 && rectPosition.y == 700) chColor(rect);
+        if (rectPosition.x < 0 || rectPosition.x > 1200 - x_side) xVelocity *= -1;
+        if (rectPosition.y < 0 || rectPosition.y > 700 - y_side) yVelocity *= -1;
 
         rect.setPosition(rectPosition);
 
