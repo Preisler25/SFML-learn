8ab45e58f3a17cd265a982a435eca0a16a58028a
Preisler <preisler.andras@gmail.com>
2023-04-04 20:17:52 +0200

    main

--- SFML learn/SFML learn.cpp
+++ SFML learn/SFML learn.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 void moveUp(sf::Vector2f&vect) {
     vect.y -= 10;
-
 }
 
 void moveDown(sf::Vector2f&vect) {
@@ -21,6 +20,7 @@ void moveRight(sf::Vector2f&vect) {
 }
 
 void chColor(sf::RectangleShape&rect) {
+    cout << rect.getPosition().x + rect.getPosition().y;
     sf::Color color = rect.getFillColor();
     if (color == sf::Color::Cyan) {
         rect.setFillColor(sf::Color::Red);
@@ -37,18 +37,18 @@ int main()
     const float x_side = 100;
     const float y_side = 100;
 
-    sf::RenderWindow window(sf::VideoMode(1200, 800), "My game");
+    sf::RenderWindow window(sf::VideoMode(800, 800), "My game");
     window.setFramerateLimit(60);
     
     sf::RectangleShape rect;
 
-    sf::Vector2f rectPosition(600, 400);
+    sf::Vector2f rectPosition(400, 400);
 
     rect.setSize(sf::Vector2f(x_side, y_side));
 
 
-    float xVelocity = 100;
-    float yVelocity = 100;
+    float xVelocity = 10;
+    float yVelocity = 10;
 
 
     while (window.isOpen())
@@ -70,19 +70,22 @@ int main()
 
 
         }
+        
+        if (rectPosition.x == 0 && rectPosition.y == 00) chColor(rect);
+        if (rectPosition.x == 800 - x_side && rectPosition.y == 00) chColor(rect);
+        if (rectPosition.x == 0 && rectPosition.y == 800 - y_side) chColor(rect);
+        if (rectPosition.x == 800 - x_side && rectPosition.y == 800 - y_side) chColor(rect);
+
 
         //physics       
-        if (rectPosition.x < 0 || rectPosition.x > 1200 - x_side) xVelocity *= -1;
+        if (rectPosition.x < 0 || rectPosition.x > 800 - x_side) xVelocity *= -1;
         if (rectPosition.y < 0 || rectPosition.y > 800 - y_side) yVelocity *= -1;
-        if (rectPosition.x == 0 && rectPosition.y == 00) chColor(rect);
-        if (rectPosition.x == 1200 && rectPosition.y == 00) chColor(rect);
-        if (rectPosition.x == 0 && rectPosition.y == 800) chColor(rect);
-        if (rectPosition.x == 1200 && rectPosition.y == 700) chColor(rect);
-
-        rect.setPosition(rectPosition);
 
         rectPosition.x += xVelocity;
         rectPosition.y += yVelocity;
+        rect.setPosition(rectPosition);
+        
+        
 
         //render
         window.clear();
